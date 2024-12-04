4b3e63ac415d4962edab784a16185ed770bca119
Noccis <toniwincent83@gmail.com>
2024-12-04 18:40:33 +0100

    Created basic note page

---
+++ src/pages/NotesPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotesPage = () => {
+  return (
+    <div id='notes-page' className='main-padding'>
+      <h3>I mitt huvud:</h3>
+      <p>Här ska det finnas en lista med "memos"</p>
+      <p>Det ska även finnas en knapp som kan lägga till memos</p>
+      <p>Och man ska kunna ta bort memos</p>
+    </div>
+  )
+}
+
+export default NotesPage;
