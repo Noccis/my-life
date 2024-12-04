e7a8ffc58406a1eb7914712fc3250bfeaf96341a
Noccis <toniwincent83@gmail.com>
2024-12-04 19:42:45 +0100

    added firebase database and authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import './App.css'
 import Header from './components/Header'
 import { Routes, Route} from "react-router-dom"
-import Landing from './pages/LandingPage'
-import Notes from './pages/NotesPage'
 import LandingPage from './pages/LandingPage'
 import NotesPage from './pages/NotesPage'
+import PrivateRoute from './components/PrivateRoute';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <div id='all-content' className='flex-column'>
+      
       <Header />
+      <AuthProvider>
       <main>
         <Routes>
-          <Route path='/' element={ <LandingPage /> }/>
-          <Route path='/notes' element={ <NotesPage /> } />
+        <Route path="/" element={<LandingPage />} />
+      <Route
+        path="/notes"
+        element={
+          <PrivateRoute>
+            <NotesPage />
+          </PrivateRoute>
+        }
+      />
         </Routes>
       </main>
+      </AuthProvider>
     </div>
   )
 }
