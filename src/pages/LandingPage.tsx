310647e7d9a0cd1481668d9808463e61b1b0b0fd
Toni Löf <89582846+Noccis@users.noreply.github.com>
2024-12-05 22:50:59 +0100

    Made some design changes to landing page and removed firebase testcomponent (#7)

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import Authenticator from '../components/Authenticator';
-import WeekOverview from '../components/WeekOverview';
+import React from "react";
+import Authenticator from "../components/Authenticator";
+import WeekOverview from "../components/WeekOverview";
+import { useAuth } from "../context/AuthContext";
 
 const LandingPage = () => {
+  const { user } = useAuth();
+
   return (
-    <div id='landing-page' className='main-padding'>
-        <h3>Landing page.</h3>
+    <div
+      id="landing-page"
+      className="main-padding flex-row flex-align-center content-containers"
+    >
+      {user ? (
         <WeekOverview />
-        <Authenticator />
+      ) : (
+        <div>
+          <h3 className="margin-bottom-large">
+            Välkommen! Här kan du logga in.
+          </h3>
+          <Authenticator />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default LandingPage;
