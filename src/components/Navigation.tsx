2ee59d5c83bff34098063ea55ccd3f8fb17e824c
Noccis <toniwincent83@gmail.com>
2024-12-07 15:33:07 +0100

    changed the design of the week-overview component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,7 @@ import "../styling/navigation.css"
 const Navigation = () => {
   return (
     <div id='navigation-container'>
-        <ul className='flex-row'>
+        <ul className='flex-row list-style-none'>
             <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/notes">Notes</NavLink></li>
             <li><NavLink to="/profile">Profile</NavLink></li>
