b3521fa1a453b266c7e3c0befe280712a282d73b
Toni Löf <89582846+Noccis@users.noreply.github.com>
2024-12-16 14:42:42 +0100

    Hotfix/remove imports (#16)
    * Created basic note page (#1)
    
    * Firebase connection (#2)
    
    * added firebase database and authentication
    
    * changed landingpage so that user can login
    
    * created basic profile page (#3)
    
    * Note page displaying notes from firebase and you can add note to firebase to (#4)
    
    * changes to Auth (#5)
    
    * Week overview (#6)
    
    Created a weekly overview for the user.
    
    * Made some design changes to landing page and removed firebase testcomponent (#7)
    
    * Fix/landing page (#8)
    
    * Made some design changes to landing page and removed firebase testcomponent
    
    * changed the design of the week-overview component (#9)
    
    * Created a footer (#10)
    
    * Small design change to notes page (#11)
    
    * formated the files (#12)
    
    * Feature/env (#14)
    
    * Added env file
    
    * added config file to git
    
    * removed unused imports

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "../styling/footer.css"
 
 const Footer = () => {
--- src/components/WeeklyQuote.tsx
+++ src/components/WeeklyQuote.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const WeeklyQuote = () => {
   return (
     <div>WeeklyQuote</div>
