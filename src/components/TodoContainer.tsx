b74d4fa40903a9a4f723fbd96989c75130a2af62
Toni Löf <89582846+Noccis@users.noreply.github.com>
2024-12-23 16:21:37 +0100

    Created todo components to show, add and delete daily todo (#18)

---
+++ src/components/TodoContainer.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import { db, auth } from '../configuration';
+import { collection, onSnapshot, deleteDoc, doc } from 'firebase/firestore';
+import AddTodo from './AddTodo';
+
+interface Todo {
+    id: string;
+    text: string;
+    createdAt: string;
+}
+
+const TodoContainer: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const user = auth.currentUser;
+
+    if (user) {
+      const todosCollection = collection(db, `users/${user.uid}/todos`);
+      const unsubscribe = onSnapshot(todosCollection, (snapshot) => {
+        const todosData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        })) as Todo[];
+        setTodos(todosData);
+      });
+
+      return () => unsubscribe(); // Rensa lyssnaren vid avmontering
+    }
+  }, []);
+
+    const deleteTodo = async (id: string) => {
+      const user = auth.currentUser;
+      if (user) {
+        try {
+          const todosDoc = doc(db, `users/${user.uid}/todos`, id);
+          await deleteDoc(todosDoc);
+        } catch (error) {
+          console.error('Error deleting todo:', error);
+        }
+      }
+    };
+
+  return (
+    <div className='max-width-500'>
+        <h2>Dagens todos:</h2>
+        <AddTodo />
+        <ul className="notes-list">
+        {todos.map((todo) => (
+            <li key={todo.id} className="note-item">
+                <span className="note-text">{todo.text}</span>
+                <button
+            className="delete-btn"
+            onClick={() => deleteTodo(todo.id)}
+            aria-label="Delete Todo"
+          >
+            🗑️
+          </button>
+            </li>
+        ))}
+        </ul>
+    </div>
+  )
+}
+
+export default TodoContainer
--- src/pages/NotesPage.tsx
+++ src/pages/NotesPage.tsx
@@ -1,10 +1,9 @@
 import AddNote from '../components/AddNote';
 import NotesList from '../components/NotesList';
-import "../styling/notesPage.css"
 
 const NotesPage = () => {
   return (
-    <div id='notes-page' className='content-containers main-padding'>
+    <div id='notes-page' className='content-containers main-padding max-width-500'>
       <h2 className='margin-t-b'>I mitt huvud just nu:</h2>
       <AddNote />
       <NotesList />
